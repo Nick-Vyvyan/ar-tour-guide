927c0acbbf8ff59e2e4e6b9a0bdeef86e1163233
Ethan-Blight <blighte@wwu.edu>
2022-09-28 22:47:48 -0700

    Added Google Maps integration. Building outlining not yet implemented.

--- AdminApp/artgadminapp/src/views/PanelView/index.js
+++ AdminApp/artgadminapp/src/views/PanelView/index.js
@@ -1,63 +1,52 @@
 // react imports
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { Navigate } from "react-router-dom";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import useAuth from "../../hook/useAuth";
 import Error from "../../components/Error";
 
 // components
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const PanelView = (props) => {
   // auth and error
   const { user } = useAuth();
-  // eslint-disable-next-line
-  const [error, setError] = useState(null);
-
-  const [xCoord, setXCoord] = useState("");
-  const [yCoord, setYCoord] = useState("");
-  const [websiteLink, setWebsiteLink] = useState("");
 
   if (user === null || user === undefined)
     return <Navigate to={{ pathname: "/login" }} />;
 
+  // eslint-disable-next-line
+  const [error, setError] = useState(null);
+  const [websiteLink, setWebsiteLink] = useState("");
+
+  // center point for WWU on Google map
+  const wwuCenter = useMemo(
+    () => ({
+      lat: 48.73438903,
+      lng: -122.48639749,
+    }),
+    []
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC4KhgTKzblt28kngclW9__A2vZUevgdgo",
+  });
+  if (!isLoaded) return <div>Loading Map...</div>;
+
   return (
     <Container style={{ marginTop: "80px" }}>
-      <h1>Enter Information to Add a Building/Landmark</h1>
+      <h1>Add a Building/Landmark</h1>
       <br />
       <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>GPS Coordinates</Form.Label>
-          <Row>
-            <Col>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Enter GPS x-coordinate..."
-                value={xCoord}
-                onChange={(e) =>
-                  e.target.value.match(/^[0-9]*[.,]?[0-9]*$/) &&
-                  setXCoord(e.target.value)
-                }
-              />
-            </Col>
-            <Col>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Enter GPS y-coordinate..."
-                value={yCoord}
-                onChange={(e) =>
-                  e.target.value.match(/^[0-9]*[.,]?[0-9]*$/) &&
-                  setYCoord(e.target.value)
-                }
-              />
-            </Col>
-          </Row>
-        </Form.Group>
+        <Form.Label>Outline Building/Landmark on Map:</Form.Label>
+        <GoogleMap
+          zoom={15}
+          center={wwuCenter}
+          mapContainerClassName="map-container"
+        />
+        <br />
 
         <Form.Group className="mb-3">
           <Form.Label>Website Link</Form.Label>
@@ -81,6 +70,7 @@ const PanelView = (props) => {
           Submit
         </Button>
       </Form>
+      <br />
 
       {/* display all errors */}
       {error && <Error error={error} />}
