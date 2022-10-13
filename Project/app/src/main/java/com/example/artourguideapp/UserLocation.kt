f083bd210b03801dc03d1cb7211e5c80632ca8f4
Nick-Vyvyan <vyvyannick@gmail.com>
2022-10-12 17:00:25 -0700

    require context in constructor
    now uses provided context to access the fusedLocationProvider
    
    BUG - app crashes when permissions are denied

--- Project/app/src/main/java/com/example/artourguideapp/UserLocation.kt
+++ Project/app/src/main/java/com/example/artourguideapp/UserLocation.kt
@@ -2,11 +2,14 @@ package com.example.artourguideapp
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
 
 
@@ -14,12 +17,15 @@ private const val DEFAULT_UPDATE_INTERVAL : Long= 15
 private const val FAST_UPDATE_INTERVAL : Long = 5
 private const val PERMISSIONS_FINE_LOCATION = 101
 
-class UserLocation : Activity() {
+class UserLocation(
+    private val context: Context
+){
     private lateinit var currentLocation : Location
-
     private var locationRequest: LocationRequest = LocationRequest()
     private var locationCallback: LocationCallback
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var onLocationUpdate: ((LocationResult) -> Unit)? = null
 
     init {
         // config location request
@@ -30,10 +36,12 @@ class UserLocation : Activity() {
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(result: LocationResult) {
                 super.onLocationResult(result)
-
+                onLocationUpdate?.invoke(result)
                 currentLocation = result.lastLocation!!
             }
         }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
 
         startLocationUpdates()
     }
@@ -42,39 +50,46 @@ class UserLocation : Activity() {
         return currentLocation
     }
 
+    fun setLocationUpdateListener(listener : (LocationResult) -> Unit) {
+        onLocationUpdate = listener
+    }
+
     fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
+        if (ContextCompat.checkSelfPermission(
+                context,
                 Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
+        ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                context,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         ) != PackageManager.PERMISSION_GRANTED) {
             // permissions not granted
-           ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_FINE_LOCATION)
+           requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_FINE_LOCATION)
 1        }
 
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        when (requestCode) {
-            PERMISSIONS_FINE_LOCATION -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    updateGPS()
-                } else {
-                    Toast.makeText(this, "This app requires location permissions to function properly", Toast.LENGTH_SHORT).show()
-                    finish()
-                }
-            }
-        }
-    }
+
+    /**WILL BE NEEDED IN (MAIN ACTIVITY??)**/
+//    fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        when (requestCode) {
+//            PERMISSIONS_FINE_LOCATION -> {
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    updateGPS()
+//                } else {
+//                    Toast.makeText(this, "This app requires location permissions to function properly", Toast.LENGTH_SHORT).show()
+//                    finish()
+//                }
+//            }
+//        }
+//    }
 
 
     fun stopLocationUpdates() {
@@ -82,9 +97,9 @@ class UserLocation : Activity() {
     }
 
     private fun updateGPS() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.lastLocation.addOnSuccessListener { location : Location? ->
                 if (location != null) {
                     currentLocation = location
@@ -92,7 +107,7 @@ class UserLocation : Activity() {
             }
         } else { // fine location permission not granted
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_FINE_LOCATION)
+                requestPermissions(context as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_FINE_LOCATION)
             }
         }
     }
