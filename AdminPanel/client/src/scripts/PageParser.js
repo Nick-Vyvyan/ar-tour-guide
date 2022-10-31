da68d45cde99a71ee4b121a91fb4ac2292a08848
goodnic <goodnic@wwu.edu>
2022-10-31 01:18:29 -0700

    added gender neutral restrooms and accessibilty

--- AdminPanel/client/src/scripts/PageParser.js
+++ AdminPanel/client/src/scripts/PageParser.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-function parse(page) {
+function parseWeb(page) {
   const output = {};
   const data = fs.readFileSync(page, "utf8");
 
@@ -64,6 +64,18 @@ function parse(page) {
     arr[i] = removeTagsFromString(v);
   });
 
+  // get gender neutral restrooms
+  output.genderNeutralRestrooms = parseRoomNumbers(
+    getContentBetweenTags('<p>', '</p>',
+      getContentBetweenTags(
+        'class="field field--name-field-gender-neutral-restrooms field--type-text-long field--label-hidden field-item">', 
+        '</div>', 
+        data
+      )
+    )
+  )
+
+  // get dining data
   const diningData = getValuesAsList(
     getContentBetweenTags(
       '<div class="field field--name-field-dining field--type-text-long field--label-hidden field-item">',
@@ -82,88 +94,14 @@ function parse(page) {
     });
   }
 
-  return output;
-}
-
-function parseWeb(data) {
-  const output = {};
-
-  output.buildingName = getContentBetweenTags(
-    "<span>",
-    "</span>",
-    getContentBetweenTags('<h1 class="page-title">', "</h1>", data)
-  );
-
-  output.buildingTypes = getContentBetweenTags(
-    '<span class="field-content">',
-    "</span",
-    data
-  ).split(", ");
-
-  const floorPlanElement = getParentTag(
-    '<span class="material-icons" aria-hidden="true">domain</span>',
-    data
-  );
-  if (floorPlanElement !== null) {
-    output.floorplanLink = getContentBetweenTags(
-      'href="',
-      '"',
-      floorPlanElement
-    );
-  }
-
-  // get Departments and Offices
-  const departmentsOfficesElements = getDivListContent(
-    'href="https://www.wwu.edu/taxonomy',
-    data
-  );
-
-  if (departmentsOfficesElements) {
-    departmentsOfficesElements.forEach(
-      (s, i, arr) =>
-        (arr[i] = s
-          .substring(s.search(">") + 1)
-          .substring(0, s.substring(s.search(">") + 1).search("<"))
-          .trim())
-    );
-  }
-
-  output.departmentsOffices = departmentsOfficesElements;
-
-  // get computer labs
-  output.computerLabs = getValuesAsList(
+  // get accessibilty info
+  output.accessibilityInfo = getListContent(
     getContentBetweenTags(
-      '<div class="field field--name-field-computer-labs field--type-text-long field--label-hidden field-item">',
-      "</div>",
+      'class="field field--name-field-accessibility field--type-text-long field--label-hidden field-item">',
+      '</div>',
       data
     )
-  );
-
-  if (!output.computerLabs) {
-    output.computerLabs = [];
-  }
-
-  output.computerLabs.forEach((v, i, arr) => {
-    arr[i] = removeTagsFromString(v);
-  });
-
-  const diningData = getValuesAsList(
-    getContentBetweenTags(
-      '<div class="field field--name-field-dining field--type-text-long field--label-hidden field-item">',
-      "</div>",
-      data
-    )
-  );
-
-  output.dining = [];
-
-  if (diningData) {
-    diningData.forEach((v) => {
-      const diningLink = getContentBetweenTags('href="', '">', v);
-      const diningName = getContentBetweenTags('">', "</a>", v).trim();
-      output.dining.push([diningName, diningLink]);
-    });
-  }
+  )
 
   return output;
 }
@@ -208,24 +146,43 @@ function getValuesAsList(data) {
   }
 }
 
-// get content between li tags in an unordered list
+// get content between li tags in an unordered list including sublists
 function getListContent(data) {
-  const ret = [];
+  let ret = [];
 
-  let listElements = getContentBetweenTags("<ul>", "</ul>", data);
+  if (! data) { return [] }
 
-  if (listElements === null) {
-    return null;
-  }
+  let listElements = data.substring(4, data.length-5)
 
   while (listElements.indexOf("<li>") >= 0) {
-    ret.push(
-      listElements.substring(
-        listElements.indexOf("<li>") + 4,
-        listElements.indexOf("</li>")
+    // if found ul in current list item
+    if (listElements.indexOf('<ul>') != -1 && listElements.indexOf('<ul>') < listElements.indexOf('</li>')) {
+      // call func recursively on inside unordered list
+      ret = ret.concat(
+        getListContent(
+          listElements.substring(
+            listElements.indexOf("<ul>"), 
+            listElements.indexOf("</ul>")+5
+          )
+        )
       )
-    );
-    listElements = listElements.substring(listElements.indexOf("</li>") + 4);
+
+      listElements = listElements.substring(listElements.indexOf("</ul>") + 10) 
+    } else {
+
+      ret.push(
+        String(
+          listElements.substring(
+            listElements.indexOf("<li>") + 4,
+            listElements.indexOf("</li>")
+          )
+        )
+      )
+
+      listElements = listElements.substring(listElements.indexOf("</li>") + 4);
+    }
+
+    
   }
 
   return ret;
@@ -286,4 +243,26 @@ function removeTagsFromString(data) {
   return retString;
 }
 
-module.exports = { parse, parseWeb };
+// gets string of room numbers and coverts to list
+function parseRoomNumbers(data) {
+
+  if (!data) {
+    return []
+  }
+
+  // get past initial non-numerical chars
+  i = 0
+  while (data[i].charCodeAt(0) < 48 || data[i].charCodeAt(0) > 57){
+    i++
+  }
+
+  data = data.substring(i)
+
+  if (!data) {
+    return []
+  }
+
+  return data.split(', ')
+}
+
+module.exports = { parseWeb };
