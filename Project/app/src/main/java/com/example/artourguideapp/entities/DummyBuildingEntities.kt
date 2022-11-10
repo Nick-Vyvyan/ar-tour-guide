31fc3fdc0fa648b15076958a71c146ccab10d8e2
Nick-Vyvyan <vyvyannick@gmail.com>
2022-11-10 00:03:25 -0800

    add more dummy buildings

--- Project/app/src/main/java/com/example/artourguideapp/entities/DummyBuildingEntities.kt
+++ Project/app/src/main/java/com/example/artourguideapp/entities/DummyBuildingEntities.kt
@@ -1,9 +1,7 @@
 package com.example.artourguideapp.entities
 
-import android.app.Application
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
-import com.example.artourguideapp.MainActivity
 
 /**
  * This class serves as a holder for Building Entities until we get data loaded in
@@ -13,15 +11,17 @@ class DummyBuildingEntities() {
     companion object {
         var cfLoc : Location = Location("Communications Facility")
         var wkrcLoc : Location = Location("Wade King Recreational Center")
+        var awLoc : Location = Location("Academic West")
+        var esLoc = Location("Environmental Studies")
 
         var h1Loc : Location = Location("House 1")
         var h2Loc : Location = Location("House 2")
         var h3Loc : Location = Location("House 3")
         var h4Loc : Location = Location("House 4")
         var h5Loc : Location = Location("House 5")
+        var stairLoc : Location = Location("Stairs")
 
         var entityList = ArrayList<Entity>()
-
 
         var commFacilityEntity = BuildingEntity(
             "Communications Facility",
@@ -85,6 +85,18 @@ class DummyBuildingEntities() {
                     "swinging bed with the steel girders of modern technology. The fact that the bed was removed soon after the sculpture was erected does not diminish the sculpture's " +
                     "impact or meaning.", "", "https://westerngallery.wwu.edu/mark-di-suvero-handel-1975"
         )
+        )
+
+        var stairsToNowhere = LandmarkEntity(
+            "Stairs To Nowhere",
+            ArrayList<Location>(),
+            stairLoc,
+            LandmarkData(
+                "Stairs to Nowhere",
+                "tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus",
+                "tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus",
+                "https://westerngallery.wwu.edu/mark-di-suvero-handel-1975"
+            )
         )
 
 
@@ -217,6 +229,58 @@ class DummyBuildingEntities() {
                 "https://www.wwu.edu/building/cf")
         )
 
+        var academicWest = BuildingEntity(
+            "Academic West",
+            ArrayList<Location>(),
+            awLoc,
+            BuildingData(
+                "Academic West",
+                "AW",
+                "Educational",
+                "Communication Studies\nComputer Science\nJournalism\nPhysics and Astronomy",
+                "Button activated entrances are located on the east and west sides of the building" +
+                        "\nThere are accessible restrooms located on all floors" +
+                        "\nCentrally located elevators provide access to all floors" +
+                        "\nAccessible parking is available to the east (Lot 17 G)",
+                "CF 157",
+                "CF 21\n" +
+                        "CF 24\n" +
+                        "CF 26\n" +
+                        "CF 161\n" +
+                        "CF 165\n" +
+                        "CF 167\n" +
+                        "CF 312",
+                "",
+                "",
+                "https://www.wwu.edu/building/aw")
+        )
+
+        var environmentalStudies = BuildingEntity(
+            "Environmental Studies",
+            ArrayList<Location>(),
+            esLoc,
+            BuildingData(
+                "Environmental Studies",
+                "AW",
+                "Educational",
+                "Environmental Studies\nComputer Science\nJournalism\nPhysics and Astronomy",
+                "Button activated entrances are located on the east and west sides of the building" +
+                        "\nThere are accessible restrooms located on all floors" +
+                        "\nCentrally located elevators provide access to all floors" +
+                        "\nAccessible parking is available to the east (Lot 17 G)",
+                "CF 157",
+                "CF 21\n" +
+                        "CF 24\n" +
+                        "CF 26\n" +
+                        "CF 161\n" +
+                        "CF 165\n" +
+                        "CF 167\n" +
+                        "CF 312",
+                "",
+                "",
+                "https://www.wwu.edu/building/es")
+        )
+
 
         fun initialize(activity: AppCompatActivity) {
             cfLoc.latitude = 48.7327738818
@@ -224,6 +288,12 @@ class DummyBuildingEntities() {
 
             wkrcLoc.latitude = 48.7315959997
             wkrcLoc.longitude = -122.488958036
+
+            awLoc.latitude = 48.73228015834756
+            awLoc.longitude = -122.48651712172678
+
+            esLoc.latitude = 48.73343638692885
+            esLoc.longitude =  -122.48551791080642
 
             h1Loc.latitude = 48.88876780964667
             h1Loc.longitude = -122.47953104095755
@@ -240,13 +310,19 @@ class DummyBuildingEntities() {
             h5Loc.latitude = 48.76244375674259
             h5Loc.longitude = -122.45055761431342
 
+            stairLoc.latitude = 48.73259523326817
+            stairLoc.longitude = -122.48624102397639
+
             entityList.add(commFacilityEntity)
             entityList.add(wadeKingEntity)
+            entityList.add(academicWest)
+            entityList.add(environmentalStudies)
             entityList.add(house1)
             entityList.add(house2)
             entityList.add(house3)
             entityList.add(house4)
             entityList.add(house5)
+            entityList.add(stairsToNowhere)
 
             for (entity in entityList) {
                 entity.initNode(activity)
