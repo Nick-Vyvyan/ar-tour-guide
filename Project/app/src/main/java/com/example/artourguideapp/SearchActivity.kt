a7ed185f3b3f431f70c73e819d5e3021ba22577e
Ethan-Blight <blighte@wwu.edu>
2022-11-18 17:39:57 -0800

    Minor tweaks

--- Project/app/src/main/java/com/example/artourguideapp/SearchActivity.kt
+++ Project/app/src/main/java/com/example/artourguideapp/SearchActivity.kt
@@ -14,6 +14,7 @@ var originalEntities: ArrayList<Entity> = DummyBuildingEntities.entityList
 var currentEntities: ArrayList<Entity> = ArrayList()
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -29,6 +30,7 @@ class SearchActivity : AppCompatActivity() {
         }
 
         // handle search
+        currentEntities.clear()
         currentEntities.addAll(originalEntities)
         findViewById<Button>(R.id.searchButton).setOnClickListener {
             refreshEntityList()
