ab836f8dd594e3bacb173c142ef0fbe2eb17333e
Nick-Vyvyan <vyvyannick@gmail.com>
2023-01-24 16:14:11 -0800

    add title bar
    also make all in-menu buttons the lighter blue

--- Project/app/src/main/java/com/example/artourguideapp/navigation/NavigationArrowNode.kt
+++ Project/app/src/main/java/com/example/artourguideapp/navigation/NavigationArrowNode.kt
@@ -34,7 +34,7 @@ class NavigationArrowNode(var activity: Activity, var currentWaypoint: Navigatio
     init {
         // Build arrow node
         ModelRenderable.builder()
-            .setSource(activity, R.raw.wwu_arrow)
+            .setSource(activity, R.raw.wwu_arrow_light)
             .setIsFilamentGltf(true)
             .build()
             .thenAccept { newRenderable ->
